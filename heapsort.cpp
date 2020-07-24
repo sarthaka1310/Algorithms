5763609ef77bcafb5aad9c16a72e3403b92445a2
sarthaka1310 <56290744+sarthaka1310@users.noreply.github.com>
2020-07-24 21:40:48 +0530

    Corrected indexation errors

--- heapsort.cpp
+++ heapsort.cpp
@@ -1,5 +1,4 @@
-// Heapsort and Min heap in C++
-
+// Min heap in C++
 #include<iostream>
 #include<vector>
 
@@ -9,12 +8,13 @@ using namespace std;
 
 void heapify(int i, vector<ll> &v)
 {
-	if(!v.size())
-		return;
-	if(i>=(v.size()+1)/2)
+	if(v.empty() || 2*i+1>=v.size() || i<0)
 		return;
 
-	int k=v[2*i]<v[(2*i)+1]?(2*i):(2*i+1);
+	// int k=v[2*i+1]<v[(2*i)+2]?(2*i+1):(2*i+2);
+	int k= 2*i+1;
+	if(k+1<v.size())
+		k+=int(v[k]>v[k+1]);
 
 	if(v[i]>v[k]){
 	swap(v[i], v[k]);
@@ -24,7 +24,7 @@ void heapify(int i, vector<ll> &v)
 
 void build_heap(vector<ll> &v)
 {
-	if(!v.size())
+	if(v.empty())
 		return;
 	int i=(v.size()+1)/2;
 	while(i--)
@@ -33,26 +33,26 @@ void build_heap(vector<ll> &v)
 
 void insert(ll n, vector<ll> &v)
 {
-	if(!v.size())
+	if(v.empty())
 		return;
 
 	v.push_back(n);
 	int i=v.size()-1;
 	
-	while(i && v[i/2]>v[i])
+	while(i && v[(i-1)/2]>v[i])
 	{
-		swap(v[i], v[i/2]);
-		i/=2;
+		swap(v[i], v[(i-1)/2]);
+		i=(i-1)/2;
 	}
 }
 
 ll extract_min(vector<ll> &v)
 {
-	if(!v.size())
+	if(v.empty())
 		return -1;
 
 	ll min=v[0];
-	v[0]=v[v.size()-1];
+	v[0]=v.back();
 	v.pop_back();
 	heapify(0, v);
 
@@ -60,7 +60,7 @@ ll extract_min(vector<ll> &v)
 }
 
 ll find_min(const vector<ll> v){
-	return v.size()?v[0]:-1;
+	return v.empty()?-1:v[0];
 }
 
 vector<ll> heap_sort(vector<ll> v)
